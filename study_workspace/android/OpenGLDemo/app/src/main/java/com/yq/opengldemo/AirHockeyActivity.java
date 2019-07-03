1fb5dad393fb592475ed8b5c1bb4cc3072fb41d3
yangqiang <yangqiang149@163.com>
2019-07-03 11:59:03 +0800

    加入w坐标

--- study_workspace/android/OpenGLDemo/app/src/main/java/com/yq/opengldemo/AirHockeyActivity.java
+++ study_workspace/android/OpenGLDemo/app/src/main/java/com/yq/opengldemo/AirHockeyActivity.java
@@ -37,7 +37,7 @@ public class AirHockeyActivity extends AppCompatActivity {
 
         private static final int BYTES_PER_FLOAT = 4;
         //x and y for a point for tableVerticesWithTrangles
-        private static final int POSITION_COMPONENT_COUNT = 2;
+        private static final int POSITION_COMPONENT_COUNT = 4;
         private static final int COLOR_COMPONENT_COUNT = 3;
         /*The stride tells OpenGL the interval between each position or each color.
         As we now have both a position and a color attribute in the same data array, OpenGL can no
@@ -70,20 +70,20 @@ public class AirHockeyActivity extends AppCompatActivity {
                 //order of coordinates: x, y, r, g, b
 
                 // Triangle Fan
-                0,      0,      1f,     1f,     1f,
-                -0.5f,  -0.8f,  0.7f,   0.7f,   0.7f,
-                0.5f,   -0.8f,  0.7f,   0.7f,   0.7f,
-                0.5f,   0.8f,   0.7f,   0.7f,   0.7f,
-                -0.5f,  0.8f,   0.7f,   0.7f,   0.7f,
-                -0.5f,  -0.8f,  0.7f,   0.7f,   0.7f,
+                0,      0,      0f,     1.5f,   1f,     1f,     1f,
+                -0.5f,  -0.8f,  0f,     1f,     0.7f,   0.7f,   0.7f,
+                0.5f,   -0.8f,  0f,     1f,     0.7f,   0.7f,   0.7f,
+                0.5f,   0.8f,   0f,     2f,     0.7f,   0.7f,   0.7f,
+                -0.5f,  0.8f,   0f,     2f,     0.7f,   0.7f,   0.7f,
+                -0.5f,  -0.8f,  0f,     1f,     0.7f,   0.7f,   0.7f,
 
                 //Line 1
-                -0.5f,  0f,     1f,     0f,     0f,
-                0.5f,   0f,     1f,     0f,     0f,
+                -0.5f,  0f,     0f,     1.5f,   1f,     0f,     0f,
+                0.5f,   0f,     0f,     1.5f,   1f,     0f,     0f,
 
                 //Mallets
-                0f,     -0.4f, 0f,     0f,     1f,
-                0f,     0.4f,  1f,     0f,     0f,
+                0f,     -0.4f,  0f,     1.25f,  0f,     0f,     1f,
+                0f,     0.4f,   0f,     1.75f,  1f,     0f,     0f,
             };
 
             vertexData = ByteBuffer
